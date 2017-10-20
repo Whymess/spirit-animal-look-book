d5276632ac4d0e8c800de4b510ef76d85a06dffa
Whymess <Jonathan.lev.messing@gmail.com>
2017-10-19 21:04:36 -0400

    user shows

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+   apiKey: 'AIzaSyCH3LQRAc1pxUFSH4n2W2LUC3nO6AumztA',
+    authDomain: 'spirit-animal-look-book-c92ea.firebaseapp.com',
+    databaseURL: 'https://spirit-animal-look-book-c92ea.firebaseio.com',
+    projectId: 'spirit-animal-look-book-c92ea',
+    storageBucket: '',
+    messagingSenderId: '446258458595'
 };
 
 firebase.initializeApp(config);
